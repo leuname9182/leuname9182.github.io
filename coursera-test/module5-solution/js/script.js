1475f5663672566ac905009d8bbf8a76d130f068
unknown <leuname9182@gmail.com>
2024-12-01 21:58:17 +0800

    Module5TestUpdate

--- coursera-test/module5-solution/js/script.js
+++ coursera-test/module5-solution/js/script.js
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       //
       // var homeHtmlToInsertIntoMainPage = ....
 
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName");
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
